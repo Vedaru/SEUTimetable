05aab328fb48956488c75562b9353320cac89582
SakiMidare <ssmzhn@163.com>
2026-02-01 21:32:57 +0800

    chore: 优化import

--- app/src/main/java/top/sakimidare/seutimetable/ui/news/info/InfoComponents.kt
+++ app/src/main/java/top/sakimidare/seutimetable/ui/news/info/InfoComponents.kt
@@ -49,7 +49,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
